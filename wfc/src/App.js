49ddecc977bf31bde2127037c4b853de152d1e2c
joshwilcken <joshuawilcken@Joshuas-MacBook-Pro.local>
2017-07-13 09:10:06 -0600

    added slider

--- wfc/src/App.js
+++ wfc/src/App.js
@@ -1,12 +1,15 @@
 //the App.js file will serve as a routing app
 
 import React, { Component } from 'react';
+import Slider from './Container/Slider/SliderView'
 
 class App extends Component {
   render() {
     return (
       <div>
         GROUP PROJECT!!!
+
+        <Slider />
       </div>
     );
   }
