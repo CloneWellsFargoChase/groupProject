e8577198ff4e3356ea10e72621d3d90c30945db5
vostersc <vosters01@gmail.com>
2017-07-13 09:29:25 -0600

    finished: new user and login endpoints

--- wfc/server/server.js
+++ wfc/server/server.js
@@ -3,7 +3,7 @@ const express = require('express'),
       app = express(),
       bodyParser = require('body-parser'),
       massive = require('massive'),
-      controller = require('../server/controllersNode/controller1'),
+      controller = require('./controllersNode/controller1'),
       cors = require('cors'),
       axios = require('axios'),
       port = 3007;
@@ -14,6 +14,7 @@ const express = require('express'),
 app.use(bodyParser.json());
 app.use(express.static('../public'));
 app.use(cors());
+app.set('trust_proxy', 1); //remove if we dont set up NGINX
 
 
 ////// DATABASE //////
@@ -23,7 +24,32 @@ massive(config.database).then(db => {
 
 
 ////// ENDPOINTS //////
-app.get('/test', controller.test)
+
+
+// done
+// pass in fName, lName, userName, password, email
+app.post('/newUser', controller.checkIfUserExists, controller.newUser, controller.login)
+
+
+// done
+// pass in password & username or email
+app.post('/login', controller.login)
+
+
+// pass in from username and to username & email
+app.get('/transactions', controller.transactions)
+
+
+// pass in userName and email for target acct
+app.get('/xfer', controller.checkIfUserExists, controller.xfer)
+
+
+// pass in searchValue
+app.get('/transactions/:val', controller.tsearch)
+
+
+// pass in
+app.get('/forgot', controller.forgot)
 
 
 ////// LISTENING //////
