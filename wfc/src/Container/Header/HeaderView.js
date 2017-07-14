16ddca41ba904d2a8d7bb2b4f7c0361a52d5cf50
CHASE <t.chasepettyjohn@yahoo.com>
2017-07-14 09:55:54 -0600

    more styling on header

--- wfc/src/Container/Header/HeaderView.js
+++ wfc/src/Container/Header/HeaderView.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import hamburger from '../../Images/Icons/hamburger.svg';
 import arrow from '../../Images/Icons/down-arrow.svg';
+import magnify from '../../Images/Icons/magnifying-glass.svg';
+import text from '../../Images/Icons/chase-text.svg';
+import logo from '../../Images/Icons/chase-logo.svg';
 import './Header.css';
 
 class Header extends Component {
@@ -11,13 +14,39 @@ class Header extends Component {
           <div className="header-left">
               <div className="menu-dropdown">
                 <img src={hamburger} className="hamburger-bars" />
-                  Explore products
-              <img src={arrow} className="menu-dropdown-arrow" />
+                 <div className="dropdown">
+                 <button className="dropdown-button">
+                    <div className="dropdown-text"> 
+                      Explore products
+                    </div>
+                  <img src={arrow} className="menu-dropdown-arrow" />
+                  </button>
+                  <div className="dropdown-content">
+                    <img className='dropdown-icon' src={require("../../Images/Icons/credit-card-back-symbol.svg")}/>
+                    Credit cards
+                    <img className='dropdown-icon' src={require("../../Images/Icons/check-with-pen.svg")} />
+                    
+                    <img className='dropdown-icon' src={require("../../Images/Icons/wallet.svg")} />
+                    <img className='dropdown-icon' src={require("../../Images/Icons/credit-card-with-plus-sign.svg")} />
+                    <img className='dropdown-icon' src={require("../../Images/Icons/key.svg")} />
+                    <img className='dropdown-icon' src={require("../../Images/Icons/house.svg")} />
+                    <img className='dropdown-icon' src={require("../../Images/Icons/line-chart.svg")} />
+                    <img className='dropdown-icon' src={require("../../Images/Icons/briefcase.svg")} />
+                    <img className='dropdown-icon' src={require("../../Images/Icons/skyline.svg")} />
+                    <img className='dropdown-icon' src={require("../../Images/Icons/more-button-of-circular-shape.svg")} />
+                  </div>
+                  </div>
             </div>
           </div>
         <div className="header-middle">
+          <img src={text} className="chase-text" />
+          <img src={logo} className="chase-logo" />
         </div>
-        <div className="header-right"></div>
+          <div className="header-right">
+            <div className="atm">ATM & branch</div>
+            <div className="español">Español</div>
+            <img src={magnify} className="magnifying-glass" />
+          </div>
         </div>
       </div>
     );
