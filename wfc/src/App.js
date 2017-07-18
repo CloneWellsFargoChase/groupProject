b0a6a652acdd423865e32704c6cab7221ed98de9
joshwilcken <joshwilcken@gmail.com>
2017-07-18 17:02:10 -0600

    committing todays changes

--- wfc/src/App.js
+++ wfc/src/App.js
@@ -15,7 +15,6 @@ class App extends Component {
   render() {
     return (
       <div>
-      GroupProject!
           <Switch>
             <Route exact path="/" component={Home}/>
             <Route path="/account" component={Account}/>
--- wfc/src/Page/SignUp.js
+++ wfc/src/Page/SignUp.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import SignUpComponent from '../Container/SignUp/SignUpView';
+
+
 
 class SignUp extends Component {
     render() {
         return (
-            <div>
-                test signup
-            </div>
+            <MuiThemeProvider>
+                <div>
+                    <SignUpComponent />
+                </div>
+            </MuiThemeProvider>
         );
     }
 }
