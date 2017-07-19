5f04f890a01f16d4ccb0a757f33d8031652b31cb
vostersc <vosters01@gmail.com>
2017-07-19 12:37:24 -0600

    before merge

--- wfc/src/Container/Body/AccountBodyLeft.js
+++ wfc/src/Container/Body/AccountBodyLeft.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
-import './AccountBodyLeft.css';
 
 export default class AccountBodyLeft extends Component {
     render() {
         return (
-            <div>
-                <div className="AccountBodyLeftDiv">Left</div>
+            <div className="AccountBodyLeft">
+
+                <div className="AccountBodyLeftTop">
+                  <div>TOTAL CHECKING</div>
+                  <div>
+                    <div>$amnt</div>
+                    <div>Available balance</div>
+                  </div>
+                </div>
+
+                <div className="AccountBodyLeftBottom">
+
+                  <div className="AccountBodyLeftBottomImg">cardIMG</div>
+
+                  <div className="AccountBodyLeftBottomText">
+                    <div className="AccountBodyLeftBottomTextHead">The quick & simple card finder</div>
+                    <div>
+                    In just 2 steps, find the card that fits your needs.
+                    </div>
+                    <div className="AccountBodyLeftBottomTextLink">Find your card > </div>
+                  </div>
+
+                </div>
+
             </div>
         );
     }
